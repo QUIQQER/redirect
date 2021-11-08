82004405a1571fc152aec308175c0c7c2cc43fdc
Jan Wennrich <jan.wennrich@pcsg.de>
2021-11-08 13:46:36 +0100

    feat: adds method to generate children array for add redirect dialog

--- src/QUI/Redirect/Utils.php
+++ src/QUI/Redirect/Utils.php
@@ -10,133 +10,45 @@ namespace QUI\Redirect;
  * Class Url
  * @package redirect\src\QUI\Redirect
  */
-class Url
+class Utils
 {
-    /**
-     * Prepares an URL to be used as a source URL.
-     * This means:
-     *   - only leaving the URLs' path behind
-     *   - prepending a slash
-     *   - removing languages from the path
-     *
-     * @param string $url - The URL to be prepared
-     *
-     * @return string
-     */
-    public static function prepareSourceUrl($url)
-    {
-        $url = static::getRelevantPartsFromUrl($url);
-        $url = static::stripLanguageFromPath($url);
+    public static function makeChildrenArrayForAddRedirectDialog(
+        array $childrenUrls,
+        string $parentSourceUrl = '',
+        string $parentTargetUrl = ''
+    ): array {
+        $result = [];
 
-        return $url;
-    }
+        foreach ($childrenUrls as $childUrl) {
+            $target = '';
 
-    /**
-     * Returns the parts from a given url that are relevant for a redirect.
-     * This may include the path, the query and the fragment.
-     *
-     * @param $url
-     *
-     * @return string
-     */
-    public static function getRelevantPartsFromUrl($url): string
-    {
-        $urlParts = parse_url($url);
+            if ($parentSourceUrl && $parentTargetUrl) {
+                // Check if $childUrl begins with $parentSourceUrl
+                if (substr($childUrl, 0, strlen($parentSourceUrl)) === $parentSourceUrl) {
+                    $replacement = $parentTargetUrl;
 
-        if (!isset($urlParts['path'])) {
-            return '';
-        }
+                    // Replacing with just a slash, results in double slashes in the beginning -> replace with nothing
+                    if ($replacement === '/') {
+                        $replacement = '';
+                    }
 
-        $result = '/' . trim($urlParts['path'], '/');
+                    // Replace the beginning of $childUrl with $parentTargetUrl
+                    // We can't use a "global" str_replace as this might replace parts at the end of the url too
+                    $target = substr_replace(
+                        $childUrl,
+                        $replacement,
+                        0,
+                        strlen($parentSourceUrl)
+                    );
+                }
+            }
 
-        if (isset($urlParts['query'])) {
-            $result .= '?' . $urlParts['query'];
-        }
-
-        if (isset($urlParts['fragment'])) {
-            $result .= '#' . $urlParts['fragment'];
+            $result[] = [
+                'source' => $childUrl,
+                'target' => $target
+            ];
         }
 
         return $result;
-    }
-
-
-    public static function stripLanguageFromPath($url)
-    {
-        // Remove all slashes from the beginning and end of the path
-        $url = trim($url, '/');
-
-        // Strip language parts from the URL (e.g. "/en/mysite" becomes "/mysite"
-        // Check strlen = 2 if we have a root URL (e.g. "/en/" becomes just "en" on this check)
-        // Since site-names can't be two characters long, it's definitely a language
-        if (strpos($url, '/') === 2 || strlen($url) === 2) {
-            $url = substr($url, 3);
-        }
-
-        // Append a slash
-        $url = "/" . $url;
-
-        return $url;
-    }
-
-
-    /**
-     * Prepares an URL to be used as an internal target URL.
-     * This means only leaving the URLs' path behind and prepending a slash.
-     *
-     * @param string $url - The URL to be prepared
-     *
-     * @return string
-     */
-    public static function prepareInternalTargetUrl($url)
-    {
-        $path = static::getPath($url);
-        $path = self::stripLanguageFromPath($path);
-
-        return $path;
-    }
-
-
-    /**
-     * Returns if a given URL is an internal URL (starting with "index.php?id=")
-     *
-     * @param string $url - The URL to test
-     *
-     * @return boolean
-     */
-    public static function isInternal($url)
-    {
-        return strpos($url, 'index.php?id=') === 0;
-    }
-
-
-    /**
-     * Returns the path of an URL with a prepended slash.
-     * When the second parameter is set to false no slash will be prepended.
-     *
-     * @param $url - The URL which's path should be returned
-     * @param $prependSlash - Return the path with a prepended slash
-     *
-     * @return string
-     */
-    public static function getPath($url, $prependSlash = true)
-    {
-        $url = parse_url($url, PHP_URL_PATH);
-
-        if ($prependSlash && strpos($url, "/") !== 0) {
-            $url = "/" . $url;
-        }
-
-        if (!$prependSlash && strpos($url, "/") === 0) {
-            $url = substr_replace($url, '', 0, 1);
-        }
-
-        return $url;
-    }
-
-
-    public static function getQueryString($url)
-    {
-        return parse_url($url, PHP_URL_QUERY);
     }
 }
