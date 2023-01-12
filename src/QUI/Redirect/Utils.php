9f8c3a531e4748c5b7e20fac7e8f7acdabc811f5
Jan Wennrich <jan.wennrich@pcsg.de>
2023-01-12 14:30:47 +0100

    feat: adds method to generate child source URL from parent URLs
    Generates a child source URL from the child target URL and its parent source and target URLs.
    Using this method the "old" child URL (source URL for the redirect) does not have to be stored.
    It can be generated on the fly which requires less overhead.

--- src/QUI/Redirect/Utils.php
+++ src/QUI/Redirect/Utils.php
@@ -6,6 +6,10 @@
 
 namespace QUI\Redirect;
 
+use function strlen;
+use function substr;
+use function substr_replace;
+
 /**
  * Class Url
  * @package redirect\src\QUI\Redirect
@@ -50,5 +54,44 @@ class Utils
         }
 
         return $result;
+    }
+
+    /**
+     * Generates a child source URL from the child target URL and its parent source and target URLs.
+     * Using this method the "old" child URL (source URL for the redirect) does not have to be stored.
+     * It can be generated on the fly which requires less overhead.
+     *
+     * @param string $childTargetUrl
+     * @param string $parentSourceUrl
+     * @param string $parentTargetUrl
+     *
+     * @return string
+     */
+    public static function generateChildSourceUrlFromParentRedirectUrls(
+        string $childTargetUrl,
+        string $parentSourceUrl,
+        string $parentTargetUrl
+    ): string {
+        $replacement = $parentSourceUrl;
+
+        // Replacing with just a slash, results in double slashes in the beginning -> replace with nothing
+        if ($replacement === '/') {
+            $replacement = '';
+        }
+
+        // Replace the beginning of $childUrl with $parentSourceUrl
+        // We can't use a "global" str_replace as this might replace parts at the end of the url too
+        // Therefore we replace just the length of $parentTargetUrl
+        // Example:
+        // Parent Source: /hello
+        // Parent Target: /hi
+        // Child Target: /hi/world
+        // -> Child Source: /hello/world
+        return substr_replace(
+            $childTargetUrl,
+            $replacement,
+            0,
+            strlen($parentTargetUrl)
+        );
     }
 }
--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\TestCase;
 use QUI\Redirect\TemporaryStorage;
 use QUI\Redirect\TestUtil;
 use QUI\Redirect\Url;
+use QUI\Redirect\Utils;
 
 final class UtilsTest extends TestCase
 {
@@ -40,5 +41,43 @@ final class UtilsTest extends TestCase
             'source' =>  '/foo/b/c/foo/d',
             'target' =>  '/bar/b/c/foo/d'
         ], $childrenArray);
+    }
+
+    public function testGenerateChildSourceUrlFromParentRedirectUrls(): void
+    {
+        $this->assertEquals(
+            '/hello/world',
+            Utils::generateChildSourceUrlFromParentRedirectUrls('/hi/world', '/hello', '/hi')
+        );
+
+        $this->assertEquals(
+            '/hello/world/test',
+            Utils::generateChildSourceUrlFromParentRedirectUrls('/hi/world/test', '/hello/world', '/hi/world')
+        );
+
+        $this->assertEquals(
+            '/hello/world/test/123',
+            Utils::generateChildSourceUrlFromParentRedirectUrls('/hi/world/test/123', '/hello/world/test', '/hi/world/test')
+        );
+
+        $this->assertEquals(
+            '/hello/hello',
+            Utils::generateChildSourceUrlFromParentRedirectUrls('/hi/hello', '/hello', '/hi')
+        );
+
+        $this->assertEquals(
+            '/hi/hello',
+            Utils::generateChildSourceUrlFromParentRedirectUrls('/hello/hello', '/hi', '/hello')
+        );
+
+        $this->assertEquals(
+            '/hello/world/test',
+            Utils::generateChildSourceUrlFromParentRedirectUrls('/world/test', '/hello/world', '/world')
+        );
+
+        $this->assertEquals(
+            '/hello/world',
+            Utils::generateChildSourceUrlFromParentRedirectUrls('/hi/world', '/hello', '/hi')
+        );
     }
 }
